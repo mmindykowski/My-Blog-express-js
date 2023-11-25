dca9cc62d506e625ea97d4a528ca78b80e7f470f
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-11-25 18:35:06 +0100

    konfiguracja silnika handlebars - podstawa

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const hbs = require("express-handlebars");
 
-app.use(express.static("public"));
+app.use("/files", express.static("public"));
+
+app.engine("hbs", hbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
