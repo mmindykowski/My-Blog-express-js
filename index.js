e9543d973fd8adf2532991b7c85fcd4952715143
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-11-25 17:31:48 +0100

    warunek wyświetlenia danych usera

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ app.get("/", function (req, res) {
   res.send("Hello Nodemon!");
 });
 
-app.get('/user/:id', function (req, res) {
-    res.send('user ' + req.params.id)
-  })
+app.get("/user/:id?", function (req, res) {
+  if (req.params.id) {
+    res.send("user" + req.params.id);
+  } else {
+    res.send("All users");
+  }
+});
 
 app.listen(8080, function () {
   console.log("Serwer Node.js działa");
