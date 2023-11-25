d2c3bbedd1f27b7d963587baa365c36fb5818e14
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-11-25 18:15:05 +0100

    serwowanie statycznych danych

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  res.send("Hello Nodemon!");
+  res.send("Hello World!");
 });
 
-app.get("/user/:id?/:name?", function (req, res) {
-  if (req.params.id) {
-    res.send("user" + req.params.id + '' + req.params.name);
-  } else {
-    res.send("All users");
-  }
-});
+// app.get("/user/:id?/:name?", function (req, res) {
+//   if (req.params.id) {
+//     res.send("user" + req.params.id + "" + req.params.name);
+//   } else {
+//     res.send("All users");
+//   }
+// });
 
-
-app.get("/data", function (req, res) {
-    if (req.query.search) {
-      res.send('Wyszukaj' + req.query.search + '' + req.query.model );
-    } else {
-      res.send("Błąd");
-    }
-  });
+// app.get("/data", function (req, res) {
+//   if (req.query.search) {
+//     res.send("Wyszukaj" + req.query.search + "" + req.query.model);
+//   } else {
+//     res.send("Błąd");
+//   }
+// });
 
 app.listen(8080, function () {
   console.log("Serwer Node.js działa");
