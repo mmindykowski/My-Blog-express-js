370e87afddff6aa3518f8960c0b9f1ad7f0f851d
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-11-29 21:03:28 +0100

    stała postController

--- app/router/blogRouter.js
+++ app/router/blogRouter.js
@@ -1,2 +1,20 @@
 const express = require("express");
 const router = express.Router();
+
+const postController = require("./app/controllers/postController");
+
+app.get("/blog", postController.index);
+
+app.get("/blog/add", (_req, res) => {
+  res.render("blogViews/addPost");
+});
+
+app.post("/blog/add", postController.create);
+
+app.get("/blog/:id", postController.post);
+
+app.get("/blog/edit/:id", postController.editForm);
+
+app.post("/blog/edit/:id", postController.update);
+
+app.get("/blog/delete/:id", postController.delete);
--- index.js
+++ index.js
@@ -56,22 +56,6 @@ app.get("/", function (_req, res) {
 //   }
 // });
 
-app.get("/blog", postController.index);
-
-app.get("/blog/add", (_req, res) => {
-  res.render("blogViews/addPost");
-});
-
-app.post("/blog/add", postController.create);
-
-app.get("/blog/:id", postController.post);
-
-app.get("/blog/edit/:id", postController.editForm);
-
-app.post("/blog/edit/:id", postController.update);
-
-app.get("/blog/delete/:id", postController.delete);
-
 app.listen(8080, function () {
   console.log("Serwer Node.js działa");
 });
