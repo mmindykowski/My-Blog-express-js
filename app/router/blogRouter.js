220039e534e46bd9af0243d20f4c141d1bedae44
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-12-05 21:16:02 +0100

    eksport

--- app/router/blogRouter.js
+++ app/router/blogRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const postController = require("../controllers/postController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/", postController.index);
 
