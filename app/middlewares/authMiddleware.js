fb9cc5c1c3a64588667c5eb30e53208d8ee5e873
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-12-06 20:51:34 +0100

    weryfikacja uzytkownika

--- app/middlewares/authMiddleware.js
+++ app/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/UserModel");
 
 module.exports = (req, res, next) => {
   const token = req.cookies["AuthToken"];
@@ -9,7 +10,16 @@ module.exports = (req, res, next) => {
     try {
       const verified = jwt.verify(token, "secretKey");
       console.log(verified);
-      next();
+
+      User.findById(verified._id)
+        .then((user) => {
+          res.locals.userId = verified._id;
+          res.locals.userName = user.name;
+          next();
+        })
+        .catch((err) => {
+          res.send(err);
+        });
     } catch {
       res.redirect("/user/login");
     }
