cb832e9c0e03ea04c025f2d003cfd3e019cf6c71
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-12-12 18:50:22 +0100

    post po id

--- app/api/postApiController.js
+++ app/api/postApiController.js
@@ -19,7 +19,7 @@ module.exports = {
       .populate("author")
       .lean()
       .then((post) => {
-        res.status(200).json(posts);
+        res.status(200).json(post);
       })
       .catch((err) => {
         res.status(404).json({ error: "Resource not found" });
