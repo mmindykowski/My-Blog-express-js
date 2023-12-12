773f2eba10e0bdfefd3335cdc338b4243c785083
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-12-12 19:47:37 +0100

    dodanie api

--- app/router/blogApiRouter.js
+++ app/router/blogApiRouter.js
@@ -6,5 +6,6 @@ const postApiController = require("../api/postApiController");
 router.get("/", postApiController.index);
 router.get("/:id", postApiController.post);
 router.post("/", postApiController.create);
+router.put("/:id", postApiController.update);
 
 module.exports = router;
