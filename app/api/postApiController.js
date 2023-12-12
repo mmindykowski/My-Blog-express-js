42b9076d39847326fc26439cb13b54fc156c880c
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-12-12 18:44:23 +0100

    modyfikacja

--- app/api/postApiController.js
+++ app/api/postApiController.js
@@ -14,4 +14,15 @@ module.exports = {
         res.status(500).json({ error: err });
       });
   },
+  post: (req, res) => {
+    Post.findById(req.params.id)
+      .populate("author")
+      .lean()
+      .then((post) => {
+        res.status(200).json(posts);
+      })
+      .catch((err) => {
+        res.status(404).json({ error: "Resource not found" });
+      });
+  },
 };
--- app/router/blogApiRouter.js
+++ app/router/blogApiRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postApiController = require("../api/postApiController");
 
 router.get("/", postApiController.index);
+router.get("/:id", postApiController.post);
 
 module.exports = router;
