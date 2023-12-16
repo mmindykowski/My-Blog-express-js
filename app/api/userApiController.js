d4fb96c188105a937252d8522c0446b052434e79
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-12-16 21:54:08 +0100

    metoda create

--- app/api/userApiController.js
+++ app/api/userApiController.js
@@ -1,2 +1,22 @@
 const User = require("../models/UserModel");
 const bcrypt = require("bcrypt");
+
+module.exports = {
+  create: (req, res) => {
+    const newUser = User(req.body);
+    newUser
+      .save()
+      .then(() => {
+        res.redirect("/blog");
+      })
+      .catch((err) => {
+        if (err.code === 11000) {
+          res.render("userViews/signupUser", {
+            error: true,
+            message: "User already exist",
+            user: req.body,
+          });
+        }
+      });
+  },
+};
